5337cb18fdfb585ae88e13be77b265194854e513
Ilya Nabutovsky <in@isprime.com>
2019-08-11 04:12:17 -0400

    Added new controllers, routes, models, and Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# You can have the root of your site routed with "root"
+  root "posts#index"
+
+#Example of a regular route:
+
+# This is a "get" request to the "Pages" controller and the "about" view.
+get 'about' => 'pages#about'
+
+
+# Example resource reoute (maps HTTPS verbs to contoller actions)
+resources :posts
 end
